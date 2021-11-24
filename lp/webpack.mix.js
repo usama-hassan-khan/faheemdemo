2d75d636b1a9dd49efdd140f45cd117e5ed50863
Muhammad <muhammad.fahim@cooperativecomputing.com>
2021-11-24 15:11:12 +0500

    Header Menu

--- lp/webpack.mix.js
+++ lp/webpack.mix.js
@@ -11,7 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.sass('resources/assets/sass/app.scss', 'public/assets/css');
+    
+
+    mix.scripts([
+        'node_modules/jquery/dist/jquery.min.js',
+        
+        'resources/assets/js/app.js',
+        'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+
+    
+        ], 
+
+        'public/assets/js/all.js'
+    );

d3d7c48a0f7f35a3fc50aa55e45361d81366469a
Muhammad <muhammad.fahim@cooperativecomputing.com>
2021-11-24 16:03:06 +0500

    Header Changes

--- lp/resources/views/header-menu.blade.php
+++ lp/resources/views/header-menu.blade.php
@@ -46,7 +46,6 @@
   </div>
 </nav>
 
-
 <!-- <div class="main-header ">
     <div class="hero-anime">
 
@@ -54,34 +53,32 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <nav class="navbar navbar-expand-md navbar-light">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-                            <a class="navbar-brand" href="#">
-                                <img src="#" alt="">
+                            <a class="navbar-brand" href="#">Navbar</a>
 
-                            </a>
-
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
-                            </button>
+                              </button>
 
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                               
                                 <ul class="navbar-nav ml-auto py-4 py-md-0">
                                   
-                                        <li class="nav-item">
+                                    <li class="nav-item dropdown">
                                          
-                                             <a href="javascript:;" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Main Services</a>
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         
-                                         
+                                            Main Services
+                                        </a>
                                        
-                                        <div class="dropdown-menu">
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         
-                                            <a class="dropdown-item" href="#">Services</a>
-                                            <a class="dropdown-item" href="#">Services 1</a>
-                                            <a class="dropdown-item" href="#">Services 2</a>
-                                            <a class="dropdown-item" href="#">Services 3</a>
-                                            <a class="dropdown-item" href="#">Services 4</a>
-                                        </div>
+                                            <li><a class="dropdown-item" href="#">Services</a></li>                                            <li><a class="dropdown-item" href="#">Services 1</a></li>
+                                            <li><a class="dropdown-item" href="#">Services 2</a></li>
+                                            <li><a class="dropdown-item" href="#">Services 3</a></li>
+                                            <li><a class="dropdown-item" href="#">Services 4</a></li>
+                                        </ul>
                                        
                                         </li>
                                    
@@ -95,6 +92,7 @@
         </div>
     </div>
 </div> -->
+
 <script src="{{asset('assets/js/all.js') }}" type="text/javascript"></script>
 </body>
 </html>
